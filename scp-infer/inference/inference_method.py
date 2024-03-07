6170da9f419f4813935fa18ba03bd3c60aede24c
Jan Sprengel <jans@jan-xps.test>
2024-03-07 14:13:39 +0100

    adding buiding blocks

---
+++ scp-infer/inference/inference_method.py
@@ -0,0 +1,32 @@
+"""class InferenceMethod: template for inference methods"""
+import numpy as np
+
+
+class InferenceMethod:
+    """
+    Template for Inference Methods
+
+    Contains placeholders for all the utilities neccesary for implementing inference Methods.
+
+    Wanted functionalities:
+    - training for each Algorithm
+    - test/val split
+    class Methods:
+    - infer (train)
+        save trained parameters
+        return: Graph / Adjacency Matrix
+    - eval (test) 
+        return: Graph / Adjacency Matrix
+
+
+    """
+
+    def __init__(self, adata_obj) -> None:
+        pass
+
+    def convert_data(self):
+        """convert adata entries into respective format for algorithm"""
+
+    def infer(self) -> np.array:
+        """perform inference"""
+        raise NotImplementedError
