b4ac41126e811c15670bc3e725e7c5b30147d6be
Jan Sprengel <jans@jan-xps.test>
2024-03-07 15:46:48 +0100

    fix error

--- scp_infer/adata/__init__.py
+++ scp_infer/adata/__init__.py
@@ -1,6 +1,5 @@
 """data processing"""
-from .filter_adata import get_perturb_labels, create_data_matrix_gies, \
-    create_data_matrix_gies_singularized, scale_counts
+from .filter_adata import get_perturb_labels, scale_counts
 
 # visualization
 from .plot_adata import plot_perturb_vs_non, print_expression_mean_std
