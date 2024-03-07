f947e7cace1b518d11a2a824e7f0edfdeb2b188f
Jan Sprengel <jans@jan-xps.test>
2024-03-07 14:36:01 +0100

    rename folder

--- scp-infer/inference/grnboost2/grnboost2.py
+++
@@ -1,6 +0,0 @@
-"""Application of GRNBoost2 Algorithm (using arboreto)"""
-
-import numpy as np
-import pandas as pd
-from anndata import AnnData
-from arboreto.algo import grnboost2
---
+++ scp_infer/inference/__init__.py
@@ -0,0 +1,9 @@
+"""init inference modules"""
+
+# 0. Algorithm Template
+from .inference_method import InferenceMethod
+
+# 1. GRNBoost2
+
+# 2. GIES
+from .gies import *
