1399e2a000bb476727012ca91921f13c873e38c5
Jan Sprengel <jans@jan-xps.test>
2024-03-25 16:11:00 +0100

    fix mistake

--- scp_infer/utils/__init__.py
+++ scp_infer/utils/__init__.py
@@ -3,6 +3,4 @@
 # 1. data split
 from .data_split import shuffled_split, gene_holdout, total_intervention_holdout
 
-from .run_infer import save_split, load_split
-
 from .data_manage import ScpiDataManager
