16232689e82471c755eae2304660d337544ad834
Jan Sprengel <jans@jan-xps.test>
2024-03-25 14:46:01 +0100

    update to datamanager save_split

--- scp_infer/utils/data_manage.py
+++ scp_infer/utils/data_manage.py
@@ -8,7 +8,6 @@ import scanpy as sc
 import numpy as np
 
 from scp_infer.utils import shuffled_split, gene_holdout  # , total_intervention_holdout
-from scp_infer.utils import save_split
 
 
 class ScpiDataManager():
@@ -56,6 +55,41 @@ class ScpiDataManager():
         """
         return os.path.join(self.output_folder, self.dataset_name, split_version)
 
+    def save_split(self, adata, split_version, split_label) -> None:
+        """
+        Save the data split in the appropriate folder
+        saves the annotated adata object in folder hierarchy:
+        - dataset
+            - split-version 1
+                - split_label 1
+                - split_label 2
+                - ...
+
+        Parameters
+        ----------
+        adata : AnnData
+            Annotated expression data object from scanpy
+            should be fully preprocessed and ready for inference
+        split_version : str
+            Version of the split
+        split_label : str
+            Label for the split
+        output_folder : str
+            Folder to save the files in
+
+        Returns
+        -------
+        None
+
+        """
+
+        save_folder = os.path.join(self.output_folder, \
+                                   self.dataset_name, split_version, split_label)
+        os.makedirs(save_folder, exist_ok=True)
+        save_file = os.path.join(save_folder, f"{split_label}.h5ad")
+        sc.write(save_file, adata)
+        return None
+
     def store_train_test_split(self, split_version="shuffled", test_size=0.2, n_splits=1) -> None:
         """
         Create train test splits for a given split_version
@@ -94,14 +128,14 @@ class ScpiDataManager():
         if split_version == "shuffled":
             for i in range(n_splits):
                 # label with test ratio and index
-                split_label = f"%.f_test_split_{i}"%(test_size*100)
+                split_label = f"%.f_test_split_{i}" % (test_size*100)
                 shuffled_split(adata, test_frac=test_size)
-                save_split(adata, split_version, split_label)
+                self.save_split(adata, split_version, split_label)
         elif split_version == "gene-holdout":
             for i, gene in enumerate(adata.var_names[adata.var['gene_perturbed']]):
                 split_label = f"gene_{gene}"
                 gene_holdout(adata, hold_out_gene=gene)
-                save_split(adata, split_version, split_label)
+                self.save_split(adata, split_version, split_label)
         elif split_version == "total-intervention":
             raise NotImplementedError("Not implemented yet")
         else:
