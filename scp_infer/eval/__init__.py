2c4a427dd3238bb0f64d828790adf06fbdcb5b8c
Jan Sprengel <jans@jan-xps.test>
2024-03-26 16:22:25 +0100

    add evaluation automation adn saving

--- scp_infer/eval/__init__.py
+++ scp_infer/eval/__init__.py
@@ -4,3 +4,4 @@
 from .plot import plot_adjacency_matrix
 from .measures import jaccard_index
 from .stat_eval import evaluate_wasserstein, evaluate_f_o_r, de_graph_hierarchy
+from .eval_manager import EvalManager
