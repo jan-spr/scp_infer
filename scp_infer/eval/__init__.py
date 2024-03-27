ef0c232d0bbbceae01e7c50c702f318bcadd88d8
Jan Sprengel <jans@jan-xps.test>
2024-03-27 15:26:17 +0100

    update init

--- scp_infer/eval/__init__.py
+++ scp_infer/eval/__init__.py
@@ -5,3 +5,4 @@ from .plot import plot_adjacency_matrix
 from .measures import jaccard_index
 from .stat_eval import evaluate_wasserstein, evaluate_f_o_r, de_graph_hierarchy
 from .eval_manager import EvalManager
+from .consistency_eval import jaccard_pairwise
